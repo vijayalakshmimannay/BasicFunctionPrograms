72c54a23fb46788eb7671093daec19ac7e60a859
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-29 20:31:05 +0530

    [vijayalakshmi] check given number is even or odd

---
+++ FunctionalPrograms/EvenOrOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    internal class EvenOrOdd
+    {
+        public void EvenOdd()
+        {
+            Console.WriteLine("Enter a number to find whether it is Even or Odd");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("{0} is Even Number", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is Odd Number", num);
+            }
+        }
+    }
+}
+
--- FunctionalPrograms/Program.cs
+++ FunctionalPrograms/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-FunctionalPrograms.flipcoin value = new FunctionalPrograms.flipcoin();
-value.FlipCoin();
+//FunctionalPrograms.flipcoin value = new FunctionalPrograms.flipcoin();
+//value.FlipCoin();
+FunctionalPrograms.EvenOrOdd number = new FunctionalPrograms.EvenOrOdd();
+number.EvenOdd();
