3d4f900789f97c11587d727d5791a6bbfd99700e
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-29 20:21:36 +0530

    [vijayalakshmi] flipping a coin for number to repeat

---
+++ FunctionalPrograms/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+FunctionalPrograms.flipcoin value = new FunctionalPrograms.flipcoin();
+value.FlipCoin();
---
+++ FunctionalPrograms/flipcoin.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class flipcoin
+    {
+        public void FlipCoin()
+        {
+            int head = 0, tail = 0, toss, perOfHead, perOfTail;
+            Random random = new Random();
+            Console.WriteLine("Mention a number to repeat flipping of coin");
+            int count = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                toss = random.Next(0, 2);
+                if (toss == head)
+                {
+                    head++;
+                }
+                else
+                {
+                    tail++;
+                }
+            }
+            perOfHead = (head * 100) / count;
+            perOfTail = (tail * 100) / count;
+            Console.WriteLine("Heads Percentage is: {0} %", perOfHead);
+            Console.WriteLine("Tails Percentage is: {0} %", perOfTail);
+        }
+    }
+}
