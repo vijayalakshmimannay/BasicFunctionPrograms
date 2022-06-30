f6a4a732dc7cbbd87a668df2e43cd4ca65c59f34
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-30 07:57:23 +0530

    [vijayalakshmi] find the largest number of the given number

---
+++ FunctionalPrograms/LargestNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    internal class LargestNumber
+    {
+        public void LargeNum()
+        {
+            Console.WriteLine("Enter 3 numbers to find the large one");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("{0} is largest number", num1);
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("{0} is largest number", num2);
+            }
+            else
+            {
+                Console.WriteLine("{0} is largest number", num3);
+            }
+        }
+    }
+}
--- FunctionalPrograms/Program.cs
+++ FunctionalPrograms/Program.cs
@@ -1,10 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//FunctionalPrograms.flipcoin value = new FunctionalPrograms.flipcoin();
-//value.FlipCoin();
-//FunctionalPrograms.EvenOrOdd number = new FunctionalPrograms.EvenOrOdd();
-//number.EvenOdd();
-//FunctionalPrograms.Factor factors = new FunctionalPrograms.Factor();
-//factors.Factors();
-FunctionalPrograms.HarmonicNumber harmonic = new FunctionalPrograms.HarmonicNumber();
-harmonic.HarmonicValue();
+
+FunctionalPrograms.LargestNumber largest = new FunctionalPrograms.LargestNumber();
+largest.LargeNum();
