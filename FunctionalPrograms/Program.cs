d0c01b84a38b6dad51c3cd5e7011c3dcb7ab1ff7
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-30 08:32:49 +0530

    [vijayalakshmi] check the swapping of two numbers

--- FunctionalPrograms/Program.cs
+++ FunctionalPrograms/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-FunctionalPrograms.QuotientRemainder quotient = new FunctionalPrograms.QuotientRemainder();
-quotient.QuoRem();
+FunctionalPrograms.SwapTwoNumbers swap = new FunctionalPrograms.SwapTwoNumbers();
+swap.Swap();
---
+++ FunctionalPrograms/SwapTwoNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    internal class SwapTwoNumbers
+    {
+        public void Swap()
+        {
+            Console.WriteLine("Enter first number in swap");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number in swap");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int temp = num1;
+            num1 = num2;
+            Console.WriteLine("Output of Swap is {0} {1}", num1, temp);
+        }
+    }
+}
+
